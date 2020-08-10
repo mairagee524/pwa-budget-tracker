2cda436071528cceba8c3285b8717de31eb78438
mairagee524 <garciamaira524@yahoo.com>
2020-08-09 20:47:42 -0700

    modified service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "index.js", "index.js"];
+const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/styles.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
