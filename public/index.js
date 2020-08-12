cb147a2e488bdc5da2b95dd8fce7610ee675d5d8
mairagee524 <garciamaira524@yahoo.com>
2020-08-12 00:12:22 -0700

    routed indexedDB file to index html

--- public/index.js
+++ public/index.js
@@ -142,6 +142,10 @@ function sendTransaction(isAdding) {
   });
 }
 
-document.querySelector("#add-btn").onclick = () => sendTransaction(true);
+document.querySelector("#add-btn").onclick = function() {
+  sendTransaction(true);
+};
 
-document.querySelector("#sub-btn").onclick = () => sendTransaction(false);
+document.querySelector("#sub-btn").onclick = function() {
+  sendTransaction(false);
+};
